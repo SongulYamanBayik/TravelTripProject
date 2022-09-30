739e71aaf78a7230c4896a6a2f84656089f2f1ed
Songül YAMAN BAYIK <snglymn@gmail.com>
2022-09-30 12:43:03 +0300

    UpdateBlog page has been created in AdminController.

--- TravelTripProject/Controllers/AdminController.cs
+++ TravelTripProject/Controllers/AdminController.cs
@@ -32,5 +32,32 @@ namespace TravelTripProject.Controllers
             c.SaveChanges();
             return RedirectToAction("BlogList");
         }
+
+        public ActionResult DeleteBlog(int id)
+        {
+            var value = c.blogs.Find(id);
+            c.blogs.Remove(value);
+            c.SaveChanges();
+            return RedirectToAction("BlogList");
+        }
+        [HttpGet]
+        public ActionResult UpdateBlog(int id)
+        {
+            var value = c.blogs.Find(id);
+            return View("UpdateBlog",value);
+        }
+        [HttpPost]
+        public ActionResult UpdateBlog(Blog blog)
+        {
+            var value = c.blogs.Find(blog.ID);
+            value.Baslik = blog.Baslik;
+            value.ImageUrl = blog.ImageUrl;
+            value.Tarih = blog.Tarih;
+            value.Aciklama = blog.Aciklama;
+            value.LikeStatus = blog.LikeStatus;
+            c.SaveChanges();
+            return RedirectToAction("BlogList");
+        }
+
     }
 }
